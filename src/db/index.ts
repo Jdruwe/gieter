1d07447e512a36c9a7cd09076110fe2f5c5cfb30
Jeroen Druwe <jeroen.druwe@weareida.digital>
2026-04-13 15:49:35 +0200

    Upgraded better-auth

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/d1"
 import { env } from "cloudflare:workers"
 import * as authSchema from "./schema/auth"
 
-export const db = drizzle((env as Env).DB, {
+export const db = drizzle(env.DB, {
   schema: {
     ...authSchema,
   },
